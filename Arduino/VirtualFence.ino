a9db88f8f2a89c019f038ff94f15cda4cb532d62
Guilh3rme99 <87912087+Guilh3rme99@users.noreply.github.com>
2023-11-12 11:33:32 -0300

    Update VirtualFence.ino
    Add polygon safe area verification

--- Arduino/VirtualFence.ino
+++ Arduino/VirtualFence.ino
@@ -1,5 +1,13 @@
 #include "config.h"
 #include "BluetoothSerial.h"
+#include <limits.h>
+
+
+#define MIN(x,y) (x < y ? x : y)
+#define MAX(x,y) (x > y ? x : y)
+#define INSIDE 0
+#define OUTSIDE 1
+
 
 TTGOClass *watch;
 TFT_eSPI *tft;
@@ -9,14 +17,64 @@ BMA *sensor;
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
+ 
+struct Point {
+    double x, y;
+};
+ 
 BluetoothSerial SerialBT;
 
 float raio = 1;
 double lat0 = -23.6687860310263;
 double lon0 = -46.54485601780751;
 double dist = 0;
-
+int lados = 4;
 String dentro = "Fora da Area";
+
+
+Point polygon[] = { { -23.666663523968463, -46.543718761178326 }, { -23.665975667081618, -46.5439655244077 }, { -23.665474511927933, -46.542109435769405 }, { -23.666476820314173, -46.54173392650733 } };
+
+
+
+bool verificaCirulo(String la, String lo){
+  dist = sqrt(pow((la.toDouble() - (lat0)),2)+pow((lo.toDouble()-(lon0)),2));
+
+  //Serial.println(dist);
+  if (dist>raio){
+    return false;
+  }else{
+    return true;
+  }  
+}
+int InsidePolygon(Point *polygon,int N,Point p)
+{
+  int counter = 0;
+  int i;
+  double xinters;
+  Point p1,p2;
+
+  p1 = polygon[0];
+  for (i=1;i<=N;i++) {
+    p2 = polygon[i % N];
+    if (p.y > MIN(p1.y,p2.y)) {
+      if (p.y <= MAX(p1.y,p2.y)) {
+        if (p.x <= MAX(p1.x,p2.x)) {
+          if (p1.y != p2.y) {
+            xinters = (p.y-p1.y)*(p2.x-p1.x)/(p2.y-p1.y)+p1.x;
+            if (p1.x == p2.x || p.x <= xinters)
+              counter++;
+          }
+        }
+      }
+    }
+    p1 = p2;
+  }
+
+  if (counter % 2 == 0)
+    return(OUTSIDE);
+  else
+    return(INSIDE);
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -65,22 +123,27 @@ void loop() {
 
     Serial.println(lat0);  
     Serial.println(lat);
+
+    Point p = {lat.toDouble(), lon.toDouble()};
+    
     //Serial.println(lon.toDouble());
     //Serial.println(pow((lat.toDouble() - (lat0)),2)+pow((lon.toDouble()-(lon0)),2));
     
-    dist = sqrt(pow((lat.toDouble() - (lat0)),2)+pow((lon.toDouble()-(lon0)),2));
+    //dist = sqrt(pow((lat.toDouble() - (lat0)),2)+pow((lon.toDouble()-(lon0)),2));
 
     //Serial.println(dist);
+    //bool d = verificaCirculo(lat,lon);    
+    int d = InsidePolygon(polygon, lados, p);
 
-    if (dist>raio){
+    if (d == 1){
       dentro = "Fora da Area";
       tft->drawString(dentro, 25, 150, 4);
     }else{
       dentro = "Dentro da Area";
       tft->drawString(dentro, 25, 150, 4);
     }
-
     
+        
     uint8_t buf[dentro.length()];
     memcpy(buf,dentro.c_str(),dentro.length());
     SerialBT.write(buf,dentro.length());
